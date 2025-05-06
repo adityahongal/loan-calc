3f3b282558b3e1a7a769d04335c050f0ee06a76a
Aditya Hongal <adityahongalwork@gmail.com>
2025-05-06 17:11:01 +0530

    Added Lazy Loading with Loader component

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "../Layout";
-import LandingPage from "../Pages/LandingPage";
-import ExchangeRateTable from "../Components/ExchangeRates/ExchangeRateTable";
+// import LandingPage from "../Pages/LandingPage";
+// import ExchangeRateTable from "../Components/ExchangeRates/ExchangeRateTable";
+import Loader from "../utils/Loader";
+
+const LandingPage = lazy(() => import("../Pages/LandingPage"));
+const ExchangeRateTable = lazy(() =>
+  import("../Components/ExchangeRates/ExchangeRateTable")
+);
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="exchange-rate" element={<ExchangeRateTable />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="exchange-rate" element={<ExchangeRateTable />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
